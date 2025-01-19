2204c6a667a18290b10d98c57fedd83c09335949
Railton-Carvalho <railtonplayrx@gmail.com>
2025-01-19 16:25:24 -0300

    feat: Curso Controller

---
+++ src/main/java/com/muxegresso/egresso/services/impl/CursoServiceImpl.java
@@ -0,0 +1,47 @@
+package com.muxegresso.egresso.services.impl;
+
+
+import com.muxegresso.egresso.domain.Curso;
+import com.muxegresso.egresso.repositories.CursoRepository;
+import com.muxegresso.egresso.services.CursoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CursoServiceImpl implements CursoService {
+
+    @Autowired
+    CursoRepository cursoRepository;
+
+    @Transactional
+    public Curso save(@Valid Curso egresso){
+        return cursoRepository.save(egresso);
+    }
+
+    public Curso findById( Integer id){
+        Curso curso = cursoRepository.findById(id).orElseThrow(()->new RuntimeException("ID não presente no sistema."));
+        return curso;
+    }
+
+    public Page<Curso> findAll(Pageable pageable){
+        return cursoRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Curso delete(Integer id){
+        Curso curso = this.findById(id);
+        cursoRepository.delete(curso);
+        return curso;
+    }
+
+    @Transactional
+    public String update(@Valid Curso curso){
+        Curso cursoObj = this.findById(curso.getId());
+        cursoRepository.save(cursoObj);
+        return "Curso atualizado com sucesso";
+    }
+}
