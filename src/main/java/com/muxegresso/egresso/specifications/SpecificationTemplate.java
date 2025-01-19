43415386ab8ea267db444e0a9044b8efa4be4a15
Railton-Carvalho <railtonplayrx@gmail.com>
2025-01-19 15:13:13 -0300

    feat: add engresso basic specification

--- src/main/java/com/muxegresso/egresso/specifications/SpecificationTemplate.java
+++ src/main/java/com/muxegresso/egresso/specifications/SpecificationTemplate.java
@@ -1,2 +1,21 @@
-package com.muxegresso.egresso.specifications;public class SpecificationTemplate {
+package com.muxegresso.egresso.specifications;
+
+import com.muxegresso.egresso.domain.Egresso;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    //tem tbm o @Or => vai pegar um ou outro e retornar
+    @And(
+            {@Spec(path = "cpf", spec = Like.class),
+                    @Spec(path = "email", spec = Like.class),
+                    @Spec(path = "userStatus", spec = Equal.class)
+            })
+    public interface EgressoSpec extends Specification<Egresso> {
+
+    }
 }
