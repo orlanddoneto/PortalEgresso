ca41236ae49540dcb119e2197a8bb63180228193
Railton <railtonplayrx@gmail.com>
2025-03-26 20:42:25 -0300

    feat: Auth Controller

--- src/main/java/com/muxegresso/egresso/controllers/AuthController.java
+++ src/main/java/com/muxegresso/egresso/controllers/AuthController.java
@@ -4,8 +4,11 @@ import com.muxegresso.egresso.domain.ApiResponse;
 import com.muxegresso.egresso.domain.Coordenador;
 import com.muxegresso.egresso.domain.Egresso;
 import com.muxegresso.egresso.domain.dtos.AutenticationDataDto;
+import com.muxegresso.egresso.domain.enums.UserStatus;
 import com.muxegresso.egresso.infra.security.TokenJwtData;
 import com.muxegresso.egresso.infra.security.TokenService;
+import com.muxegresso.egresso.repositories.EgressoRepository;
+import com.muxegresso.egresso.services.exceptions.ResourceNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,16 +30,24 @@ public class AuthController {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private EgressoRepository egressoRepository;
+
     @PostMapping
     private ResponseEntity<ApiResponse> efetuarLogin(@RequestBody @Valid AutenticationDataDto dto){
         var authenticationToken = new UsernamePasswordAuthenticationToken(dto.data(), dto.password());
         Authentication authentication = manager.authenticate(authenticationToken);
-        // Verifica se o usuário é do tipo Egresso ou Admin
+        // Verifica se o usuario é do tipo Egresso ou Admin
         Object principal = authentication.getPrincipal();
         String tokenJWT;
+
         if (dto.role().equals("egresso")) {
+            var existEgresso = egressoRepository.findByEmail(dto.data()).orElseThrow(()-> new ResourceNotFoundException(dto.data()));
+            if(existEgresso.getUserStatus().equals(UserStatus.BLOCKED)) {
+                return ResponseEntity.ok(new ApiResponse(false ,"Egresso Bloqueado para Login!"));
+            }
             tokenJWT = tokenService.genToken((Egresso) principal);
-        } else if (dto.role().equals("admin")) {
+        } else if (dto.role().equals("coordenador")) {
             tokenJWT = tokenService.genToken((Coordenador) principal);
         } else {
             throw new RuntimeException("Tipo de usuário desconhecido");
